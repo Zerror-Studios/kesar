8256f713fb605a2a52dd637aaa2deb5d3d8d8945
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-12-11 16:46:27 +0530

    updated email for forms

--- src/pages/api/submitEmail.js
+++ src/pages/api/submitEmail.js
@@ -86,7 +86,7 @@ export default async function handler(req, res) {
 
       const mailOptions = {
         from: process.env.NEXT_PUBLIC_MAIL_EMAIL_ADDRESS,
-        to: ["ayushahirwar6673@gmail.com"],
+        to: ["sales@kesarpetroproducts.com"],
         subject: `New Request Quotation – Kesar Petroproducts Ltd.`,
         html: `
        <div class="container" style="font-family: Arial, sans-serif; color: #333; padding: 20px 0;">
