148add23d54ec4858c2a39e7c6e8eb7787a659fe
Anand Vishwakarma <anandvish1005@gmail.com>
2026-01-07 18:51:43 +0530

    Revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: false,
-  output: 'standalone',
 };
 
 export default nextConfig;
