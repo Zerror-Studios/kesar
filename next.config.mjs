d5b5d2ba22892d2c144df539c311b3aa82f1a298
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-01-12 15:04:26 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "x-forwarded-proto",
+            value: "http",
+          },
+        ],
+        destination: "https://kesarpetroproducts.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
+
+
