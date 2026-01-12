bc47b6e3c94309da75ed827ddc5a1bb71487efa5
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2026-01-12 15:28:18 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,29 @@ const nextConfig = {
         source: "/:path*",
         has: [
           {
+            type: "host",
+            value: "kesarpetroproducts.com",
+          },
+        ],
+        destination: "https://www.kesarpetroproducts.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
             type: "header",
             key: "x-forwarded-proto",
             value: "http",
           },
         ],
-        destination: "https://kesarpetroproducts.com/:path*",
+        destination: "https://www.kesarpetroproducts.com/:path*",
         permanent: true,
       },
     ];
   },
 };
+
 
 export default nextConfig;
 
